6a9749565118311dc4c1ed4e76fb5be0543250be
Yesudas S <yesudas@Yesudass-MacBook-Pro.local>
2026-02-01 14:56:53 +0530

    added robots

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'counter.php';
 
 $version = "2025.08";
 
-require_once __DIR__ . 'rate-limiter.php';
+require_once __DIR__ . '/rate-limiter.php';
 $rateLimiter = new CrawlerRateLimiter();
 $rateLimiter->checkRateLimit();
 
